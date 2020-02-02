e8405d5477f26f587194a7fd07ffb28912812e60
crispu <jose.alvarado@cimat.mx>
2020-02-02 01:38:20 -0600

    added a style

---
+++ js/automata.js
@@ -0,0 +1,13 @@
+class Automata() {
+    constructor(numStates, language, initState, transFunc, acceptStates){
+        this.numStates = numStates;
+        this.language = language;
+        this.initState = initState;
+        this.transFunc = transFunc;
+        this.acceptStates = acceptStates;
+    }
+}
+
+function new_af(){
+    return;
+}
