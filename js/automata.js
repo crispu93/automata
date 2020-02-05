a5fdeb2a6c0fd933a2ac39c51d9a4b6fcc3df16b
crispu <jose.alvarado@cimat.mx>
2020-02-04 23:03:24 -0600

    Update automata.js

--- js/automata.js
+++ js/automata.js
@@ -85,7 +85,7 @@ $('#states').change( function() {
 function transitionTable() {
     let rows = afd.getStatesNumber();
     let cols = afd.getSymbolsNumber();
-    let table_body = '<table border="1" width="150">';
+    let table_body = '<table border="1" width="270" height="200">';
     for (var i = 0; i<rows; i++) {
         table_body+='<tr>';
 
